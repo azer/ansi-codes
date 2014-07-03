f63cb846be907bda70ce0d17b17c997ba47dd391
E.Azer Koçulu <azer@kodfabrik.com>
2014-07-02 23:00:55 -0700

    use prova for testing

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
+var test = require('prova');
 var codes = require("./");
 
-it('is an object of ansi codes', function(){
-  expect(codes.red).to.equal('\033[31m');
-  expect(codes.blue).to.equal('\033[34m');
-  expect(codes.reset).to.equal('\033[0m');
-  expect(codes.reset).to.equal('\033[0m');
+test('is an object of ansi codes', function (t) {
+  t.plan(4);
+  t.equal(codes.red, '\033[31m');
+  t.equal(codes.blue, '\033[34m');
+  t.equal(codes.reset, '\033[0m');
+  t.equal(codes.reset, '\033[0m');
 });
